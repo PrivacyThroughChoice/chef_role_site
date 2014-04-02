5dd0de749eb59986d5446e7de287b58d35c8f069
Fraser Scott <admin@privacythroughchoice.org>
2014-04-02 16:56:12 +0100

    I know what I am doing. Trust me.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'chef_role_site'
 maintainer       'Fraser Scott'
 maintainer_email 'admin@privacythroughchoice.org'
-'
+
 license          'MIT'
 description      'Installs/Configures chef_role_site'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
